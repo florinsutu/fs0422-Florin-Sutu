05af793716339ea07a28e743c2ddce1607dc3b49
Sergio19702 <98604139+Sergio19702@users.noreply.github.com>
2022-08-04 15:51:30 +0200

    compito

---
+++ m2/w7/d4/compito/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express()
+const path = require('path')
+
+let fs = require('fs');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+let file = fs.readFileSync('utenti.json','utf-8');
+console.log(file)
+
+
+
+
+app.get("/chi-siamo", (req,res) => {
+    
+  //  res.sendFile(path.join(__dirname+ '/chi-siamo.html'))
+
+  res.render('utenti')
+})
+app.get("/test", (req,res) => {
+    
+    res.send(faiCose())
+})
+
+app.listen(3000,() => {
+    
+    console.log('http://127.0.0.1:3000')
+})
+
+
+
+app.get('/', (req, res) => {
+    res.render(path.join(__dirname+'/home.html'),{titolo: 'utenti.json'})
+})
+
+function faiCose(){
+    let target = document.querySelector('#target')
+    fetch('utenti.json')
+    .then(res => res.json())
+    .then(utenti => {
+        console.log('ciao')
+        for(let utente of utenti){
+            let tr = document.createElement('tr')
+            for(let prop in utente){
+                let td = document.createElement('td')
+                td.textContent = utente[prop]
+                tr.append(td)
+            }
+            target.append(tr)
+        }
+    })
+} 
+
+function tiPrego(){
+    
+}
+
