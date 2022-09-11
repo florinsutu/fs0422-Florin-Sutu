4c26654cd9777d5073e4405fc8fa5a63ae761fa8
Florin Sutu <fsutu96@gmail.com>
2022-09-12 01:11:35 +0200

    progetto

--- m3/w2/progetto/progettoAngular1/src/app/pages/todos/todos.component.ts
+++ m3/w2/progetto/progettoAngular1/src/app/pages/todos/todos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from 'src/app/models/todo';
 import { TodosService } from 'src/app/services/todos.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-todos',
@@ -27,37 +28,61 @@ export class TodosComponent implements OnInit {
 
   addToDo(todo: Todo): void {
     this.todoSvc.addToDo(todo)
-      .then(res => this.allToDoes.push(res))
+      .then(res => {
+        this.allToDoes.push(res)
+
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'New Task Created',
+          showConfirmButton: false,
+          timer: 2000
+        })
+      })
   }
 
   addToCompleted(todo: Todo): void {
-    todo = Object.assign({},todo)
+    todo = Object.assign({}, todo)
     todo.completed = !todo.completed
     this.todoSvc.editToDo(todo)
-    .then(res => {
-      let index = this.allToDoes.findIndex((todo: Todo) => todo.id == res.id)
-     this.allToDoes[index].completed = res.completed
-     //non ho usato splice per rimanere nella logica del patch: cambiare soltanto parte dell'oggetto
-     // object.assign serve perchè inspiegabilmente, limitarsi a cambiare il valore di todo.completed aggiorna allToDoes prima dei due secondi
-    })
+      .then(res => {
+        let index = this.allToDoes.findIndex((todo: Todo) => todo.id == res.id)
+        this.allToDoes[index].completed = res.completed
+        //non ho usato splice per rimanere nella logica del patch: cambiare soltanto parte dell'oggetto
+      })
   }
 
   editToDo(todo: Todo): void {
     this.todoSvc.editToDo(todo)
       .then(res => {
         let index = this.allToDoes.findIndex((todo: Todo) => todo.id == res.id)
-       this.allToDoes[index].title = res.title
-       //non ho usato splice per rimanere nella logica del patch: cambiare soltanto parte dell'oggetto
+        this.allToDoes[index].title = res.title
+        //non ho usato splice per rimanere nella logica del patch: cambiare soltanto parte dell'oggetto
+
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Task Updated',
+          showConfirmButton: false,
+          timer: 2000
+        })
       })
   }
 
   deleteToDo(todo: Todo): void {
     if (todo.id) {
       this.todoSvc.deleteToDo(todo)
-      .then(res => {
-        this.allToDoes = this.allToDoes.filter((todo: Todo) => todo.id != res.id)
-      })
+        .then(res => {
+          this.allToDoes = this.allToDoes.filter((todo: Todo) => todo.id != res.id)
+
+          Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Task Deleated',
+            showConfirmButton: false,
+            timer: 2000
+          })
+        })
     }
   }
-
 }
