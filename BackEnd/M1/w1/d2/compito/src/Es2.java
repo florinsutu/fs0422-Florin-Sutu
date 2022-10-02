8978d776acb01ff95c468fe8a25cdaf88ad642b8
Florin Sutu <fsutu96@gmail.com>
2022-10-03 00:14:43 +0200

    aggiornamento

--- BackEnd/M1/w1/d2/compito/src/Es2.java
+++ BackEnd/M1/w1/d2/compito/src/Es2.java
@@ -2,20 +2,66 @@
 public class Es2 {
 
 	public static void main(String[] args) {
+
+		Sim s = new Sim("324999888");
+		print(s);
+	}
 	
-		
-		
+	public static void print(Sim s) {
+		System.out.println("Sim: ");
 	}
 
 }
 
-class Smartphone{
+class Call{
 	
-	int number;
-	int credit;
+	private String number;
+	private double time;
 	
+	public Call(String number, double time) {
+		this.number = number;
+		this.time = time;
+	}
+}
+
+class Sim {
+
+	private String number;
+	private double credit;
+	private Call[] list;
 	
-	Smartphone(){
+
+	Sim(String number) {
+		this.number = number;
+		this.credit = 0;
+		this.list = new Call[5];
+	}
+	
+	public void stampa(){
 		
 	}
+	
+	public double getCredit() {
+		return this.credit;
+	}
+	
+	public String getNumber() {
+		return this.number;
+	}
+	
+	public Call[] getCall() {
+		return this.list;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
